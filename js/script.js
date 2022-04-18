9b50a51475af0b3f5d39c08b3aeb45900a711f0b
0ffpudding <jennaanderson00@gmail.com>
2022-04-17 19:57:09 -0500

    added links to projects

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$("#project1Button").on("click", openBW);
+$("#project2Button").on("click", openCR);
+$("#project5Button").on("click", openWhereTo);
+
+function openBW() {
+    window.open("https://jennaanderson00.github.io/weather-dashboard/");
+}
+
+function openCR() {
+    window.open("https://jennaanderson00.github.io/code-quiz/");
+}
+
+function openWhereTo() {
+    window.open("https://bennettem.github.io/where2/");
+}
