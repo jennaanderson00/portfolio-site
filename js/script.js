1215df335523279a0dd385f69d988322e413252a
0ffpudding <jennaanderson00@gmail.com>
2022-06-14 21:09:23 -0500

    added new projects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $("#project1Button").on("click", openBW);
 $("#project2Button").on("click", openCR);
 $("#project4Button").on("click", openWhereTo);
+$("#groupProject2Btn").on("click", openStreamline);
 
 function openBW() {
     window.open("https://jennaanderson00.github.io/weather-dashboard/");
@@ -12,4 +13,8 @@ function openCR() {
 
 function openWhereTo() {
     window.open("https://bennettem.github.io/where2/");
+}
+
+function openStreamline() {
+    window.open("https://peaceful-earth-43575.herokuapp.com/");
 }
